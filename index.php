4e98a1c1d208b19add8613ace94c1647f9ab1293
yamatyuu-ibuki <yamatyuu1121@gmail.com>
2020-11-25 02:15:46 +0900

    最初のコミット

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset=“UTF-8”>
+   <title>4eachblog</title>
+   <link rel="stylesheet" type="text/css" href="style.css">
+    
+</head>
+
+<body>
+    
+    <?php
+    mb_internal_encoding("utf8");
+    $pdo = new PDO("mysql:dbname=lesson1;host=localhost;","root","");
+    $stmt = $pdo->query("select * from 4each_keijiban");
+    ?>
+    
+    <div class="logo"><img src="4eachblog_logo.jpg"></div>
+<header>
+    <ul>
+        <li>トップ</li>
+        <li>プロフィール</li>
+        <li>4eachについて</li>
+        <li>登録フォーム</li>
+        <li>問い合わせ</li>
+        <li>その他</li>
+    </ul>
+</header>
+
+   <div id="main">
+      <div class="main-container">
+          
+        <div class="left">
+         <h1>プログラミングに役立つ掲示板</h1>   
+         <h2>入力フォーム</h2>
+    <form method="post" action="insert.php">
+        <div>
+            <label>ハンドルネーム</label>
+            <br>
+            <input type="text" class="text" size="35" name="handlename">
+        </div>
+            <div>
+            <label>タイトル</label>
+            <br>
+            <input type="text" class="text" size="35" name="title">
+        </div>
+        <div>
+            <label>コメント</label>
+            <br>
+            <textarea cols="35" rows="7" name="comments"></textarea>
+        </div>
+        
+        <div>
+            <input type="submit" class="submit" value="送信する">
+        </div>
+    </form>
+            
+<?php
+            while($row = $stmt->fetch()) {
+            
+            echo "<div class='kiji'>";
+            echo "<h3>".$row['title']."</h3>";
+            echo "<div class='comments'>";
+            echo $row['comments'];
+            echo "<div class='handlename'>posted by".$row['handlename']."</div>";
+            echo "</div>";
+            echo "</div>";
+        }
+?>
+            
+        </div>
+        
+       
+        <div class="right">
+            <h2>人気の記事</h2>
+                <ul>
+                    <li>PHPオススメ本</li>
+                    <li>PHP MyAdminの使い方</li>
+                    <li>今人気のエディタ TOP5</li>
+                    <li>HTMLの基礎</li>
+        </ul>
+           <h2>オススメリンク</h2>
+                <ul>
+                    <li>インターノウス株式会社</li>
+                    <li>XAMPPのダウンロード</li>
+                    <li>Eclipseのダウンロード</li>
+                    <li>Bracketsのダウンロード</li>
+        </ul>
+           <h2>カテゴリ</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>PHP</li>
+                    <li>MySQL</li>
+                    <li>JavaScript</li>
+        </ul>
+        
+        </div>
+       </div>
+    </div>
+    
+</body>
+
+
+
+
+
+
+
+<footer>
+    copyright [c] internous|4each blog the which provides A to Z about programming.
+</footer>
+
+</html>
